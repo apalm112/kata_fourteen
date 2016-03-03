a93a616f7e2198634f2e1f7459b686eaace3db2b
Will Weatherford <weatherford.william@gmail.com>
2016-03-02 16:26:21 -0800

    Started on function for creating new text and created test for length of new text.

--- trigrams.py
+++ trigrams.py
@@ -2,6 +2,16 @@
 
 
 import io
+import sys
+import random
+
+
+def main(text_file, length):
+    """Docstring."""
+    text = trigrams_input(text_file)
+    trigram_dict = trigram(text)
+    new_text = make_trigrams(trigram_dict, length)
+    return new_text
 
 
 def trigrams_input(input_file):
@@ -9,7 +19,9 @@ def trigrams_input(input_file):
     sherlock = io.open(input_file)
     return sherlock.read()
 
+
 def trigram(sherlock_string):
+    """Docstring goes here."""
     sherlock_list = sherlock_string.split()
     # first_trigram_key = ' '.join(sherlock_list[:2])
     # first_trigram_val = sherlock_list[2]
@@ -19,11 +31,28 @@ def trigram(sherlock_string):
     for index, current_word in enumerate(sherlock_list[:-1]):
         if index == 0:
             continue
-        previous_word = sherlock_list[index -1]
+        previous_word = sherlock_list[index - 1]
         trigram_key = ' '.join([previous_word, current_word])
         word_list = trigram_dict.setdefault(trigram_key, [])
         next_word = sherlock_list[index + 1]
-       # trigram_dict[' '.join([previous_word, current_word])] = [next_word]  # this does the same as line 23.
+        # trigram_dict[' '.join([previous_word, current_word])] = [next_word]  # this does the same as line 23.
         word_list.append(next_word)
 
     return trigram_dict
+
+
+def make_trigrams(trigram_dict, length):
+    key, words = random.choice(list(trigram_dict.items()))
+    trigram_sentence = ' '.join([key, random.choice(words)])
+    split_list = trigram_sentence.rsplit(' ', 2)
+
+
+
+    return trigram_sentence
+
+
+if __name__ == '__main__':
+    print(sys.argv)
+    text_file = sys.argv[1]
+    length = sys.argv[2]
+    main(text_file, length)
--- trigrams_test.py
+++ trigrams_test.py
@@ -3,21 +3,40 @@ import pytest
 
 TEXT = [('trigrams.txt', 'This is a line of text\nThis is a second text line\nHere is a third text line.\n')]
 
-TRIGRAM = [('I wish I', {'I wish': ['I']}),
-                   ('I wish I may I wish I might',
-                   {'I wish': ['I', 'I'],
-                   'wish I': ['may', 'might'],
-                   'may I': ['wish'],
-                   'I may': ['I']
-                   })]
+TRIGRAM = [
+  ('I wish I',
+   {'I wish': ['I']}),
+  ('I wish I may I wish I might',
+   {'I wish': ['I', 'I'],
+    'wish I': ['may', 'might'],
+    'may I': ['wish'],
+    'I may': ['I']
+   })
+]
+
+
+LENGTH = [
+  ('sherlock_small.txt', 200),
+  ('sherlock_small.txt', 100),
+  ('sherlock_small.txt', 20),
+  ('sherlock_small.txt', 3),
+]
+
 
 @pytest.mark.parametrize('file_in, out', TEXT)
 def test_input(file_in, out):
     from trigrams import trigrams_input
     assert trigrams_input(file_in) == out
 
+
 @pytest.mark.parametrize('trigram_key, trigram_value', TRIGRAM)
 def test_trigram(trigram_key, trigram_value):
     from trigrams import trigram
     assert trigram(trigram_key) == trigram_value
 
+
+@pytest.mark.parametrize('input_text, length', LENGTH)
+def test_trigram_length(input_text, length):
+    from trigrams import main
+    result_text = main(input_text, length)
+    assert len(result_text.split(' ')) == length
